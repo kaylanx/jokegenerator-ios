5d688ab755b2090516ac37c9ac180a10336f208e
Andy Kayley <bitbucket@kayley.name>
2023-05-17 13:03:14 +0100

    Initial Commit

---
+++ SwiftUIAndTDDExample/SwiftUIAndTDDExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAndTDDExampleApp.swift
+//  SwiftUIAndTDDExample
+//
+//  Created by Andy Kayley on 17/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAndTDDExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
