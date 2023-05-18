565ead63efcf53110a1284396c930b79f7180c69
Andy Kayley <bitbucket@kayley.name>
2023-05-18 21:52:24 +0100

    Cater for single type jokes

--- SwiftUIAndTDDExampleTests/Network/NetworkJokeRepositoryTest.swift
+++ SwiftUIAndTDDExampleTests/Network/NetworkJokeRepositoryTest.swift
@@ -16,6 +16,7 @@ struct NetworkJoke: Decodable {
     let error: Bool?
     let category: String?
     let type: String?
+    let joke: String?
     let setup: String?
     let delivery: String?
     let flags: Flags?
@@ -84,10 +85,10 @@ final class NetworkJokeRepositoryTest: XCTestCase {
         stubRequestMaker.jsonJokeToReturn = """
         {
             "error": false,
-            "category": "Misc",
+            "category": "Programming",
             "type": "twopart",
-            "setup": "My wife left me because I'm too insecure and paranoid.",
-            "delivery": "Oh wait, never mind. She was just getting the mail.",
+            "setup": "Why do they call it hyper terminal?",
+            "delivery": "Too much Java.",
             "flags": {
                 "nsfw": false,
                 "religious": false,
@@ -96,7 +97,7 @@ final class NetworkJokeRepositoryTest: XCTestCase {
                 "sexist": false,
                 "explicit": false
             },
-            "id": 81,
+            "id": 226,
             "safe": true,
             "lang": "en"
         }
@@ -105,19 +106,60 @@ final class NetworkJokeRepositoryTest: XCTestCase {
         let joke = try await networkJokeRepository.joke(for: .any)
 
         XCTAssertEqual(false, joke.error)
-        XCTAssertEqual("Misc", joke.category)
+        XCTAssertEqual("Programming", joke.category)
         XCTAssertEqual("twopart", joke.type)
-        XCTAssertEqual("My wife left me because I'm too insecure and paranoid.", joke.setup)
-        XCTAssertEqual("Oh wait, never mind. She was just getting the mail.", joke.delivery)
+        XCTAssertNil(joke.joke)
+        XCTAssertEqual("Why do they call it hyper terminal?", joke.setup)
+        XCTAssertEqual("Too much Java.", joke.delivery)
         XCTAssertEqual(false, joke.flags?.nsfw)
         XCTAssertEqual(false, joke.flags?.religious)
         XCTAssertEqual(false, joke.flags?.racist)
         XCTAssertEqual(false, joke.flags?.sexist)
         XCTAssertEqual(false, joke.flags?.explicit)
 
-        XCTAssertEqual(81, joke.id)
+        XCTAssertEqual(226, joke.id)
         XCTAssertEqual(true, joke.safe)
         XCTAssertEqual("en", joke.lang)
+    }
 
+    func testSingleJokeReturnedCorrectly() async throws {
+
+        stubRequestMaker.jsonJokeToReturn = """
+        {
+            "error": false,
+            "category": "Programming",
+            "type": "single",
+            "joke": "Programming is 10% science, 20% ingenuity, and 70% getting the ingenuity to work with the science.",
+            "flags": {
+                "nsfw": false,
+                "religious": false,
+                "political": false,
+                "racist": false,
+                "sexist": false,
+                "explicit": false
+            },
+            "id": 37,
+            "safe": true,
+            "lang": "en"
+        }
+        """
+
+        let joke = try await networkJokeRepository.joke(for: .any)
+
+        XCTAssertEqual(false, joke.error)
+        XCTAssertEqual("Programming", joke.category)
+        XCTAssertEqual("single", joke.type)
+        XCTAssertEqual("Programming is 10% science, 20% ingenuity, and 70% getting the ingenuity to work with the science.", joke.joke)
+        XCTAssertNil(joke.setup)
+        XCTAssertNil(joke.delivery)
+        XCTAssertEqual(false, joke.flags?.nsfw)
+        XCTAssertEqual(false, joke.flags?.religious)
+        XCTAssertEqual(false, joke.flags?.racist)
+        XCTAssertEqual(false, joke.flags?.sexist)
+        XCTAssertEqual(false, joke.flags?.explicit)
+
+        XCTAssertEqual(37, joke.id)
+        XCTAssertEqual(true, joke.safe)
+        XCTAssertEqual("en", joke.lang)
     }
 }
